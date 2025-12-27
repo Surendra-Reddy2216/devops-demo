b238a27f4f5696436f16cbbeb906704c678a1091
Surendra Reddy <reddy110522@gmail.com>
2025-12-27 20:59:33 +0530

    adding text

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello from DevOps Pipeline!"
+    return "Hello from DevOps Pipeline! this is success"
 
 app.run(host="0.0.0.0", port=5000)
 
