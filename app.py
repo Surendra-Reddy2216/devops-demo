4e237329048119e9dacb4f4c0ff80ddb1c34e7be
Surendra Reddy <reddy110522@gmail.com>
2025-12-27 21:34:09 +0530

    final deploye

--- app.py
+++ app.py
@@ -3,7 +3,8 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello from DevOps Pipeline! this is successful surendra reddy"
+    return "Hello from DevOps Pipeline! this is successful surendra reddy thanks
+and i am so happy that i performed automation with jenkins by Ci"
 
 app.run(host="0.0.0.0", port=80)
 
