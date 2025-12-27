7e78270634597e2bb4cdf870c5465e9e7f7b0a89
Surendra Reddy <reddy110522@gmail.com>
2025-12-27 21:29:48 +0530

    new text reddy

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello from DevOps Pipeline! this is successful surendra"
+    return "Hello from DevOps Pipeline! this is successful surendra reddy"
 
 app.run(host="0.0.0.0", port=80)
 
