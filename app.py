6bbdd4f0f6b03c85315144970285e5137c25f0b5
Surendra Reddy <reddy110522@gmail.com>
2025-12-28 10:37:36 +0530

    next day deploye

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello from DevOps Pipeline! this is successful surendra reddy Thanks for thr deployment"
+    return "Hello from DevOps Pipeline! this is successful chaithu reddy Thanks for the deployment"
 
 app.run(host="0.0.0.0", port=80)
 
