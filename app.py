53e786082b5b6e4d8219ef72ad65ccaae4cf3500
Surendra Reddy <reddy110522@gmail.com>
2025-12-29 08:43:25 +0530

    rebel wood

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello from DevOps Pipeline! this is successful uday reddy Thanks for the deployment i love creating pipelines"
+    return "Hello Rebel Wood"
 
 app.run(host="0.0.0.0", port=80)
 
