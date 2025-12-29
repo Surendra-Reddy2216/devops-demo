cdde9440ab4ec8d0ca22ed245602bb527ae61213
Surendra Reddy <reddy110522@gmail.com>
2025-12-29 08:25:39 +0530

    love commit

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello from DevOps Pipeline! this is successful uday reddy Thanks for the deployment"
+    return "Hello from DevOps Pipeline! this is successful uday reddy Thanks for the deployment i love creating pipelines"
 
 app.run(host="0.0.0.0", port=80)
 
