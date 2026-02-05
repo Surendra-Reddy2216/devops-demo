56eb9775f023bf7448f5a3a0df98665b75c42600
Surendra Reddy <reddy110522@gmail.com>
2026-02-05 13:10:28 +0530

    Updated welcome message

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello Rebel Wood congrats"
+    return "Hello Rebel Wood congrats CICD is running successfully"
 
 app.run(host="0.0.0.0", port=80)
 
